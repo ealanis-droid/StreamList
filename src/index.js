708cb0beb2859696159ca51ce5e3b519cdca1cf1
Elizabeth Clausen <elizabeth.clausen@student.uagc.edu>
2025-05-11 12:32:08 -0700

    Add manifest.json and service worker dependencies. App is now installable as a PWA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+
+serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
